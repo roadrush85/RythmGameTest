bf650dff63454f8de84f5f85d39a534979b14377
roadrush85 <roadrush@icloud.com>
2017-08-28 16:13:27 +0900

    Initial commit

---
+++ Assets/Script/Manager.cs
@@ -0,0 +1,127 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Manager : MonoBehaviour {
+
+	public GameObject Note;
+
+	public GameObject[] Line;
+
+	Vector3 NotePos = new Vector3 (0f, 500f, 0);
+
+	float[,] LineDelay = {{0.5f,1f,1f,0.2f,0.6f}, {0.5f,2f,1f,0.8f,0.3f}, {0.5f,3f,1f,2f,0.6f}, {1f,3f,0.1f,2f,0.6f}};
+
+	int Point;
+	int Combo;
+	public Text Point_Text;
+	// Use this for initialization
+	public GameObject Dump;
+	public GameObject [] Particle;
+	public GameObject GameStart;
+	private static Manager manager;
+
+	public void G_Start()
+	{
+		for(int i = 0; i <4; i++)
+			StartCoroutine (LineOne (i));
+		GameStart.SetActive (false);
+		StartCoroutine (ReStart ());
+	}
+
+
+	public static Manager GetInstance()
+	{
+		return manager;
+	}
+
+	void Awake()
+	{
+		manager = this;
+	}
+
+	public void PointCheck(int _point)
+	{
+		Point += _point;
+		Point_Text.text = Point.ToString ();
+
+
+	}
+
+	void Start () {
+
+	}
+
+
+
+	IEnumerator LineOne(int _index)
+	{
+		for (int i = 0; i < 5; i++) {
+			yield return new WaitForSeconds (LineDelay [_index,i]);
+			GameObject Go = Instantiate (Note as GameObject);
+			Go.transform.SetParent(Line[_index].transform);
+			Go.transform.localPosition = NotePos;
+			Go.transform.localScale = Vector3.one;
+			Go.transform.GetComponent<Note> ().GameStart = true;
+		}
+	}
+
+	IEnumerator ReStart()
+	{
+		yield return new WaitForSeconds (10f);
+		GameStart.SetActive (true);
+	}
+
+	public void MakeNote(int _index)
+	{
+		GameObject Go = Instantiate (Note as GameObject);
+		Debug.Log (Go);
+		Go.transform.SetParent(Line[_index].transform);
+		Go.transform.localPosition = NotePos;
+		Go.transform.localScale = Vector3.one;
+		Go.transform.GetComponent<Note> ().GameStart = true;
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKeyDown (KeyCode.Q)) {
+			if (Line [0].transform.childCount > 0) 
+			{
+				Line [0].transform.GetChild(0).transform.GetComponent<Note> ().OnClick ();
+				Particle[0].SetActive (false);
+				Particle[0].SetActive (true);
+				Line [0].transform.GetChild(0).transform.parent = Dump.transform;
+			}
+			//MakeNote (0);
+		} else if (Input.GetKeyDown (KeyCode.W)) {
+			if (Line [1].transform.childCount > 0) 
+			{
+				Line [1].transform.GetChild(0).transform.GetComponent<Note> ().OnClick ();
+				Particle[1].SetActive (false);
+				Particle[1].SetActive (true);
+				Line [1].transform.GetChild(0).transform.parent = Dump.transform;
+			}
+			//MakeNote (1);
+		} else if (Input.GetKeyDown (KeyCode.E)) {
+			if (Line [2].transform.childCount > 0) 
+			{
+				Line [2].transform.GetChild(0).transform.GetComponent<Note> ().OnClick ();
+				Particle[2].SetActive (false);
+				Particle[2].SetActive (true);
+				Line [2].transform.GetChild(0).transform.parent = Dump.transform;
+			}
+			//MakeNote (2);
+		} else if (Input.GetKeyDown (KeyCode.R)) {
+			if (Line [3].transform.childCount > 0) 
+			{
+				Line [3].transform.GetChild(0).transform.GetComponent<Note> ().OnClick ();
+				Particle[3].SetActive (false);
+				Particle[3].SetActive (true);
+				Line [3].transform.GetChild(0).transform.parent = Dump.transform;
+			}
+			//MakeNote (3);
+		}
+	}
+}
---
+++ Assets/Script/Note.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Note : MonoBehaviour {
+
+	float Speed;
+	bool DesCheck;
+	public bool GameStart;
+
+	public void OnClick()
+	{
+		if (this.transform.localPosition.y > 0f) {
+			Manager.GetInstance ().PointCheck (100);
+		} else if (this.transform.localPosition.y > -100f) {
+			Manager.GetInstance ().PointCheck (200);
+		} else if (this.transform.localPosition.y > -180f) {
+			Manager.GetInstance ().PointCheck (300);
+		}
+		Destroy (this.gameObject);
+	}
+
+
+	// Use this for initialization
+	void Start () {
+		Speed = -5f;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (GameStart) {
+			this.transform.localPosition += new Vector3 (0f, Speed, 0);
+
+			if (this.transform.localPosition.y < -200f) {
+				if (!DesCheck) {
+					DesCheck = true;
+					Destroy (this.gameObject);
+				}
+			}
+		}
+	}
+}
